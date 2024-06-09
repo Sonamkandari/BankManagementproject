38ff4a20ef64e3853f4fc6341ff6bbd5c933ad43
Sonamkandari <145662567+Sonamkandari@users.noreply.github.com>
2024-06-09 13:59:03 +0530

    Add files via upload

---
+++ bankmanagementsystem.sql
@@ -0,0 +1,9 @@
+CREATE database bankmanagementsystem;
+show databases;
+use bankmanagementsystem;
+ select * from MyTable;
+ drop table Mytable;
+  CREATE table MyTable(Formno varchar(30),Name varchar(40),Fname  varchar(20),
+email varchar(40),state varchar(20),city varchar(20),address varchar(40),
+status varchar(20),G varchar(20),pincode integer);
+ 
